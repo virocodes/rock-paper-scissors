a2f58fe3a5e8dc79a0859735d3173edf2be50109
Dharshan Bellan <dbellan1291@gmail.com>
2022-05-22 14:35:15 -0700

    Create play round function

--- script.js
+++ script.js
@@ -3,3 +3,31 @@ function computerPlay() {
     let play = plays[Math.floor(Math.random() * plays.length)];
     return play;
 }
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection.toLowerCase() == 'rock') {
+        if(computerSelection == 'rock') {
+            return 'It\'s a tie! Rock and Rock';
+        } else if(computerSelection == 'paper') {
+            return 'You Lose! Paper beats Rock';
+        } else if(computerSelection == 'scissors') {
+            return 'You Win! Rock beats Scissors';
+        }
+    } else if(playerSelection.toLowerCase() == 'paper') {
+        if(computerSelection == 'rock') {
+            return 'You Win! Paper beats Rock';
+        } else if(computerSelection == 'paper') {
+            return 'It\'s a tie! Paper and Paper';
+        } else if(computerSelection == 'scissors') {
+            return 'You Lose! Scissors beats paper';
+        }
+    } else if(playerSelection.toLowerCase() == 'scissors') {
+        if(computerSelection == 'rock') {
+            return 'You Lose! Rock beats Scissors';
+        } else if(computerSelection == 'paper') {
+            return 'You Win! Scissors beats Paper';
+        } else if(computerSelection == 'scissors') {
+            return 'It\'s a tie! Scissors and Scissors';
+        }
+    }
+}
